0e71f786267b7973025fc486b8121fbf876fa13c
rm-ryou <rmco.420118@gmail.com>
2025-11-23 19:21:03 +0900

    feat: add user process
    Temporary processing to start the server

---
+++ internal/repository/user_repository.go
@@ -0,0 +1,31 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/rm-ryou/mococoplan/internal/domain"
+	"github.com/rm-ryou/mococoplan/internal/service"
+)
+
+type UserRepository struct {
+	db *sql.DB
+}
+
+func NewUserRepository(db *sql.DB) service.UserRepository {
+	return &UserRepository{
+		db: db,
+	}
+}
+
+func (ur *UserRepository) Register(ctx context.Context, user *domain.User) error {
+	return nil
+}
+
+func (ur *UserRepository) List(ctx context.Context) ([]*domain.User, error) {
+	return nil, nil
+}
+
+func (ur *UserRepository) Delete(ctx context.Context, id domain.UserID) error {
+	return nil
+}
