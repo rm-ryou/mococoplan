801b05516d8a5df6a11eef61332f493a5c313597
rm-ryou <rmco.420118@gmail.com>
2025-12-14 01:31:08 +0900

    fix: auth core interface

--- internal/core/domain/auth/interface.go
+++ internal/core/domain/auth/interface.go
@@ -3,6 +3,17 @@ package auth
 import "context"
 
 type Servicer interface {
-	Login(ctx context.Context, email, password string) error
+	Signup(ctx context.Context, cmd *SignupCmd) error
+	Login(ctx context.Context, cmd *LoginCmd) error
 	Logout(ctx context.Context) error
+}
+
+type LoginCmd struct {
+	Email    string
+	Password string
+}
+
+type SignupCmd struct {
+	Name string
+	LoginCmd
 }
