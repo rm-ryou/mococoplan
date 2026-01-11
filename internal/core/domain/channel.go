d38272cd5291db495584329863122e1eb3e0239a
rm-ryou <rmco.420118@gmail.com>
2026-01-11 22:22:56 +0900

    feat: add model

---
+++ internal/core/domain/channel.go
@@ -0,0 +1,22 @@
+package domain
+
+import "time"
+
+type Channel struct {
+	ID          int
+	WorkspaceID int
+	Name        string
+	Description string
+	IsPrivate   bool
+	CreatedBy   int
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
+
+type ChannelMember struct {
+	ChannelID int
+	UserID    int
+	JoinedAt  time.Time
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
